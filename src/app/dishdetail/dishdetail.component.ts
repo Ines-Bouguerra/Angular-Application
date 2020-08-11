d6758b28cbe107fcaf0fcfbdf430e05e94d0b198
ines <ines.bouguerra2207@gmail.com>
2020-08-11 23:34:35 +0100

    Header and Footer

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Dish } from '../shared/dish';
 
 const DISH = {
   id: '0',
@@ -50,7 +51,10 @@ const DISH = {
 
 
 export class DishdetailComponent implements OnInit {
-  dish = DISH;
+
+  @Input()
+  dish: Dish;
+
   constructor() { }
 
   ngOnInit() {
