506a412b61f155243ef6caf4cea4f570d68d5eac
ines <ines.bouguerra2207@gmail.com>
2020-08-13 11:18:29 +0100

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import {PromotionService} from "./services/promotion/promotion.service";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,7 +43,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService,PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
