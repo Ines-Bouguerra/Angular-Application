29e950c2c4ca050337e900f656acaf4ac498c5e9
ines <ines.bouguerra2207@gmail.com>
2020-08-15 17:13:28 +0100

    test Promise

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@angular/core';
-import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import {Injectable} from '@angular/core';
+import {Dish} from '../shared/dish';
+import {DISHES} from '../shared/dishes';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DishService {
 
-  constructor() { }
-  getDishes(): Dish[]{
+  constructor() {
+  }
+
+  getDishes(): Dish[] {
     return DISHES
   }
+
   getDish(id: string): Dish {
     return DISHES.filter((dish) => (dish.id === id))[0];
   }
@@ -17,4 +21,16 @@ export class DishService {
   getFeaturedDish(): Dish {
     return DISHES.filter((dish) => dish.featured)[0];
   }
+
+// getDishes(): Promise<Dish[]> {
+//   return Promise.resolve(DISHES);
+// }
+//
+// getDish(id: string): Promise<Dish> {
+//   return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+// }
+//
+// getFeaturedDish(): Promise<Dish> {
+//   return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+// }
 }
