a8b2ebf9f62dad46afa2737b352f0d709541d219
ines <ines.bouguerra2207@gmail.com>
2020-08-15 16:05:25 +0100

    <div fxFlex fxFlexOffset=20px class=form-size>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {FormsModule} from '@angular/forms';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -55,6 +57,9 @@ import {FormsModule} from '@angular/forms';
     FormsModule,
     AppRoutingModule,
     MatDialogModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+  ReactiveFormsModule
 
   ],
   entryComponents: [
